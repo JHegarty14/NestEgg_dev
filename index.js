484e616963f5c89f149490ff8eca009faedad817
Jack <jhegarty14@yahoo.com>
2017-11-09 20:05:13 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi : 'there'});
+  res.send({ hi : 'there, pissbitch'});
 });
 
 const PORT = process.env.PORT || 5000;
